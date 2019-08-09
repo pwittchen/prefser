19e7fcf5698c4f87e8a9510b9e533f6e8e9527a4
Piotr Wittchen <piotr@wittchen.io>
2019-08-09 18:42:33 +0200

    updating JavaDoc for RxJava2.x

---
+++ update_javadocs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# update javadocs for RxJava2.x
+git checkout RxJava2.x
+./gradlew clean androidJavadocs
+git checkout gh-pages
+rm -rf javadoc/RxJava2.x/*
+cp -avr library/build/docs/javadoc/* ./javadoc/RxJava2.x
+git add -A
+git commit -m "updating JavaDoc for RxJava2.x"
+rm -rf library/build/docs
+echo "javadocs for RxJava2.x updated"
+
+echo "javadocs for RxJava2.x updated - now you can push your changes"
